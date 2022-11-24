046286c682cca9b8430602fb0b523f0e269ac949
EstherJDorneles <estherjdorneles@gmail.com>
2022-11-24 13:49:15 -0300

    Adicionando classe pokemon para simplificar o uso da API

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,12 +1,30 @@
 const pokeApi = {}
 
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json(''))
+function convertPokeApiDetailToPokemon(pokeDatail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDatail.id
+    pokemon.name = pokeDatail.name
+
+    const types =  pokeDatail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type =type
+
+    pokemon.photo = pokeDatail.sprites.other.dream_world.front_default
+
+    return pokemon
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 8) => {
     const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
